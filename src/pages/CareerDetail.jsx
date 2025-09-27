f4698027045672a377937e2d8874b3c6b311ee82
tsadikum <tsadikum@gaengineering.et>
2025-09-27 19:13:32 +0300

    Update CareerDetail.jsx

--- src/pages/CareerDetail.jsx
+++ src/pages/CareerDetail.jsx
@@ -1,5 +1,23 @@
-function CareerDetail() {
-  return <div>Career Detail</div>;
-}
+import { useParams} from "react-router-dom";
+import JobDetail from "../ui/JobList/JobDetail.jsx";
+import jobData from "../data/jobsData";
+
+const CareerDetail = () => {
+  const { id } = useParams(); // URL param
+
+
+  // Find job by ID from jobData
+  const job = jobData.find((j) => j.id.toString() === id);
+
+  if (!job) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p className="text-gray-500">Job not found.</p>
+      </div>
+    );
+  }
+
+  return <JobDetail job={job} />;
+};
 
 export default CareerDetail;
