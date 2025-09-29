b8085d8cb0934d3e2871eec8eeb25601255bdecc
tsadikum <tsadikum@gaengineering.et>
2025-09-29 17:59:51 +0300

    Update Blogs.jsx

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,5 +1,11 @@
+import NewsGrid from '../ui/Blogs/NewsGrid';
+
 function Blogs() {
-  return <div>Blogs Page</div>;
+  return (
+    <div>
+      <NewsGrid />
+    </div>
+  );
 }
 
 export default Blogs;
