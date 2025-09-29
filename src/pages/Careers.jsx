0eb4fc3f54092924760bf378c2a894e24880c9b7
abdibiranu1762 <abdibiranu@gaengineering.et>
2025-09-29 10:23:43 +0300

    Update Careers.jsx

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import JobList from "../ui/JobList";
 import jobData from "../data/jobsData";
 
 const Careers = () => {
   const [jobs] = useState(jobData || []);
   const [hasMore, setHasMore] = useState(true);
-  const navigate = useNavigate();
+
 
   const handleLoadMore = () => setHasMore(false);
 
